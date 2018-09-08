3ca6c2a850853b577f371352966ff68186e67a97
AshV <ashish6318@gmail.com>
2018-09-08 22:17:20 +0530

    Add project files.

---
+++ eMiLyWebNet/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(eMiLyWebNet.Startup))]
+namespace eMiLyWebNet
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
